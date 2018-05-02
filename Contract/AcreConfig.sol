81f9850c8932245d544842ac4a2a74562f9e0a10
AcredIO <ebow@Acred.io>
2018-05-02 11:40:07 +0900

    MIN_ETHER 0.5 -> 0.1 CROWDSALE_DURATION_TIME 30 -> 22 days

--- Contract/AcreConfig.sol
+++ Contract/AcreConfig.sol
@@ -1,6 +1,10 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.20;
+
+import "./SafeMath.sol";
 
 contract AcreConfig {
+    using SafeMath for uint;
+    
     uint internal constant TIME_FACTOR = 1 days;
 
     // Ownable
@@ -17,27 +21,27 @@ contract AcreConfig {
     string internal constant TOKEN_SYMBOL          = "ACRE";
     uint8  internal constant TOKEN_DECIMALS        = 18;
     
-    uint   internal constant INIT_SUPPLY           = 1*1e8 * 10 ** uint(TOKEN_DECIMALS); // supply
+    uint   internal constant INITIAL_SUPPLY        = 1*1e8 * 10 ** uint(TOKEN_DECIMALS); // supply
     uint   internal constant CAPITAL_SUPPLY        = 4*1e7 * 10 ** uint(TOKEN_DECIMALS); // supply
     uint   internal constant PRE_PAYMENT_SUPPLY    = 1*1e7 * 10 ** uint(TOKEN_DECIMALS); // supply
     uint   internal constant MAX_MINING_SUPPLY     = 4*1e8 * 10 ** uint(TOKEN_DECIMALS); // supply
     
     // Sale
-    uint internal constant MIN_ETHER               = 5*1e17; // 0.5 ether
+    uint internal constant MIN_ETHER               = 1*1e17; // 0.1 ether
     uint internal constant EXCHANGE_RATE           = 1000;   // 1 eth = 1000 acre
-    uint internal constant PRESALE_DURATION_TIME   = 15;     // 5.8~5.22 
-    uint internal constant CROWDSALE_DURATION_TIME = 30;     // 6.11~7.2
+    uint internal constant PRESALE_DURATION_TIME   = 15;     // 15 days
+    uint internal constant CROWDSALE_DURATION_TIME = 22;     // 22 days
     
     // helper
     function getDays(uint _time) internal pure returns(uint) {
-        return _time / 1 days;
+        return SafeMath.div(_time, 1 days);
     }
     
     function getHours(uint _time) internal pure returns(uint) {
-        return _time / 1 hours;
+        return SafeMath.div(_time, 1 hours);
     }
     
     function getMinutes(uint _time) internal pure returns(uint) {
-        return _time / 1 minutes;
+        return SafeMath.div(_time, 1 minutes);
     }
 }
