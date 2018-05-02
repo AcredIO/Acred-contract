d6cf3b7e0a300f0e19319623c5b738c112fc22a7
AcredIO <ebow@Acred.io>
2018-05-02 11:54:46 +0900

    rename function

--- Contract/AcreToken.sol
+++ Contract/AcreToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.20;
 
 import "./ERC20.sol";
 import "./Lockable.sol";
@@ -15,8 +15,11 @@ contract AcreToken is Lockable, TokenERC20 {
     event logFrozenAccount(address indexed target, bool frozen);
     event logBurn(address indexed owner, uint value);
     event logMining(address indexed recipient, uint value);
+    event logWithdrawContractToken(address indexed owner, uint value);
     
-    function AcreToken(address _companyCapital, address _prePayment) TokenERC20(TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS, INIT_SUPPLY) public {
+    function AcreToken(address _companyCapital, address _prePayment) TokenERC20(TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS, INITIAL_SUPPLY) public {
+        require(_companyCapital != address(0));
+        require(_prePayment != address(0));
         companyCapital = _companyCapital;
         prePayment = _prePayment;
         transfer(companyCapital, CAPITAL_SUPPLY);
@@ -25,15 +28,15 @@ contract AcreToken is Lockable, TokenERC20 {
         pause(); 
     }
 
-    function _transfer(address _from, address _to, uint _value) conditionalPaused internal returns (bool success) {
-        require(!frozenAccount[_from]);                     
+    function _transfer(address _from, address _to, uint _value) whenConditionalPassing internal returns (bool success) {
+        require(!frozenAccount[_from]); // freeze                     
         require(!frozenAccount[_to]);
-        require(!isLockup(_from));
+        require(!isLockup(_from));      // lockup
         require(!isLockup(_to));
         return super._transfer(_from, _to, _value);
     }
     
-    function freezeAccount(address _target) onlyOwnersWithMaster public returns (bool success) {
+    function freezeAccount(address _target) onlyManagers public returns (bool success) {
         require(!isManageable(_target));
         require(!frozenAccount[_target]);
         frozenAccount[_target] = true;
@@ -41,18 +44,14 @@ contract AcreToken is Lockable, TokenERC20 {
         return true;
     }
     
-    function unfreezeAccount(address _target) onlyOwnersWithMaster public returns (bool success) {
+    function unfreezeAccount(address _target) onlyManagers public returns (bool success) {
         require(frozenAccount[_target]);
         frozenAccount[_target] = false;
         logFrozenAccount(_target, false);
         return true;
     }
     
-    function withdrawalToken(uint _value) onlyOwnersWithMaster public returns (bool success) {
-        return _transfer(this, msg.sender, _value);
-    }
-    
-    function burn(uint _value) onlyOwnersWithMaster public returns (bool success) {
+    function burn(uint _value) onlyManagers public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);   
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);            
         totalSupply = totalSupply.sub(_value);                      
@@ -60,11 +59,11 @@ contract AcreToken is Lockable, TokenERC20 {
         return true;
     }
     
-    function mining(address _recipient, uint _value) onlyOwnersWithMaster public returns (bool success) {
+    function mining(address _recipient, uint _value) onlyManagers public returns (bool success) {
         require(_recipient != address(0));
-        require(!isLockup(_recipient));      // lockup
         require(!frozenAccount[_recipient]); // freeze
-        require(totalMineSupply + _value <= MAX_MINING_SUPPLY);
+        require(!isLockup(_recipient));      // lockup
+        require(SafeMath.add(totalMineSupply, _value) <= MAX_MINING_SUPPLY);
         balanceOf[_recipient] = balanceOf[_recipient].add(_value);
         totalSupply = totalSupply.add(_value);
         totalMineSupply = totalMineSupply.add(_value);
@@ -72,8 +71,18 @@ contract AcreToken is Lockable, TokenERC20 {
         return true;
     }
     
-    function getContractBalance() public constant returns (uint balance) {
-        balance = balanceOf[this];
+    function withdrawContractToken(uint _value) onlyManagers public returns (bool success) {
+        _transfer(this, msg.sender, _value);
+        logWithdrawContractToken(msg.sender, _value);
+        return true;
+    }
+    
+    function getContractBalanceOf() public constant returns(uint blance) {
+        blance = balanceOf[this];
+    }
+    
+    function getRemainingMineSupply() public constant returns(uint supply) {
+        supply = MAX_MINING_SUPPLY - totalMineSupply;
     }
     
     function () public { revert(); }
