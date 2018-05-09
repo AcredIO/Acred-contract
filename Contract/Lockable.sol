ad0bf8a47185bde46323bcef4d6940aef0578ab7
AcredIO <ebow@Acred.io>
2018-05-09 14:47:51 +0900

    Delete log character

--- Contract/Lockable.sol
+++ Contract/Lockable.sol
@@ -5,12 +5,12 @@ import "./Pausable.sol";
 contract Lockable is Pausable {
     mapping (address => uint) public locked;
     
-    event logLockup(address indexed target, uint startTime, uint deadline);
+    event Lockup(address indexed target, uint startTime, uint deadline);
     
     function lockup(address _target) onlyOwner public returns (bool success) {
 	    require(!isManageable(_target));
         locked[_target] = SafeMath.add(now, SafeMath.mul(LOCKUP_DURATION_TIME, TIME_FACTOR));
-        logLockup(_target, now, locked[_target]);
+        Lockup(_target, now, locked[_target]);
         return true;
     }
     
